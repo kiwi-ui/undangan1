270b61bd8eee9945b554f579e679f8b2d498b0ff
kiwi-ui <yusnianggara@gmail.com>
2022-04-11 22:25:01 +0700

    Settingup the folder and files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import AboutCouple from "./AboutCouple/AboutCouple"
+import Date from "./Date/Date"
+import Footer from "./Footer/Footer"
+import Gift from "./Gift/Gift"
+import Hadist from "./Hadist/Hadist"
+import Header from "./Header/Header"
+import Wish from "./Wish/Wish"
+
+const App = () => {
+  return (
+    <section>
+      <AboutCouple />
+      <Date />
+      <footer />
+      <Gift />
+      <Hadist />
+      <Header />
+      <Wish />
+    </section>
+  )
+}
+
+export default App
